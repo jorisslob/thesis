e3d78aa7e68d9de34ba5af4b7680a457399d2587
Joris Slob <jorisslob@gmail.com>
2014-09-04 23:02:44 +0200

    PhD regulations state the requirements for a dissertation. Test these in the build.sh. Added an index to the thesis.

--- build.sh
+++ build.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
 
+# Generate the Thesis
 latex Thesis.tex
 bibtex Thesis
+makeindex Thesis.idx
 latex Thesis.tex
 latex Thesis.tex
 dvipdf Thesis.dvi
+pdftotext Thesis.pdf
+
+
+# Article 13.6: In principle, the dissertation should not exceed 100,000 words.
+# On behalf of the Doctorate Board, the Dean may allow this limit to be 
+# exceeded at the written request of the PhD candidate.
+words=`pdftotext Thesis.pdf - | wc -w`
+if [ $words -gt 100000 ]
+then
+    echo "$words words is too much, needs to be less than 100000."
+    exit 1
+else
+    echo "$words words is good"
+fi
+
+# Article 16.1: The dissertation should contain a title page stating the given 
+# names and family name of the author as registered at the Register Office, as 
+# well as an index and the required registers.
+if grep "Joris Slob" Thesis.txt
+then
+    echo "The name Joris Slob was found in the Thesis."    
+else
+    echo "The name Joris Slob was not found in the Thesis!"
+    exit 1    
+fi
+
+if grep "Index" Thesis.txt
+then
+    echo "An index was found in the Thesis."
+else
+    echo "No index was found in the Thesis!"
+    exit 1
+fi
--- clean.sh
+++ clean.sh
@@ -8,6 +8,10 @@ rm Thesis.log
 rm Thesis.lof
 rm Thesis.lot
 rm Thesis.out
+rm Thesis.idx
+rm Thesis.ilg
+rm Thesis.ind
 rm Thesis.ps
 rm Thesis.dvi
 rm Thesis.pdf
+rm Thesis.txt
